bae51c9222f55b5349aefa508e07083102c5f964
Dmitri Brengauz <dmitri@momus.net>
2011-06-09 08:28:19 -0600

    Initial Commit

---
+++ cat_2_spread.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# A program to combine two spreadsheets, each containing a column of
+# IP addresses, and combining them into one, each containing only one
+# instance of each IP address.
+
+# (c) 2011 Dmitri G. Brengauz for International Business Machines.
+
+# This software is released on the same terms as its components:
+# Ruby License.
+
+
+
