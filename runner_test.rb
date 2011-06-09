3e9522d8ef039133830a34a34a0bbd528b972b29
Dmitri Brengauz <dmitri@momus.net>
2011-06-09 14:27:08 -0600

    saving some work

---
+++ runner_test.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+
+require 'pp'
+
+
+
+
+
+load './cat_2_spread.rb'
+
+
+@file_one = ARGV[0]
+
+if  @file_one == nil
+  puts "Remeber to supply a file"
+  break
+end
+
+
+read_sheet= Input_Sheet.new(@file_one)
+
+pp "Read Sheet" , read_sheet.result_array
