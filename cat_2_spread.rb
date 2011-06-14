095e38d0ff04a2630bd3c868c79cb764ff1c1843
Dmitri Brengauz <dmitri@momus.net>
2011-06-14 16:30:22 -0600

    Some functionality, lots of bugs

--- cat_2_spread.rb
+++ cat_2_spread.rb
@@ -27,12 +27,10 @@ end
 # TODO: Write object specs for Input Sheet class.
 sheet1 =   Input_Sheet.new(ARGV[0])
 
-pp sheet1
+
 
 sheet2 =   Input_Sheet.new(ARGV[1])
 
-
-pp sheet2
 
 #Get the "keys" that return the correct hash values from the results array
 key1 = sheet1.result_array[0]
@@ -42,11 +40,11 @@ key2 = sheet2.result_array[0]
 #case, IP addresses, can be found:
 
 #print sheet1.result_array[1][key1]    Returns: "10.14.160.78"
-#print  sheet2.result_array[1][key2]   Returns: "129.39.231.134"
+#print  sheet2.result_array[n][key2]   Returns:  the IP in the n^th row
 
 
 
-#Pull in all the values of the key into an array for comparison.
+#Pull in all the values for the target key into an array for comparison.
 
 #The first array element is the key, so start at 1 not 0.  The other
 #elements in the array are hashes, so use the first element (the key)
@@ -85,25 +83,90 @@ while m < size_sheet2
   
 end
 
-comp_array1.each do |val|
-  puts comp_array2.include?(val).to_s + " " + val
+#Why not return the number of matches at run time? it will aid in
+#debugging at first.
+match_number = 0
+
+
+#Therefore, the output can be formated as an array of arrays, each
+#member being a row in the output CSV file.  We will be storing three:
+
+#IP addresses that are in both:
+union_array = []
+
+#IP addresses that are in ONE but not TWO
+diff_1_2 = []
+
+#IP addresses that are in TWO but not ONE
+diff_2_1 = []
+
+#The first line in each array is the header.  The columns in the union
+#array are based on the first sheet
+union_array[0] = sheet1.header_array
+diff_1_2[0]    = sheet1.header_array
+diff_2_1[0]    = sheet2.header_array
+
+
+union_line = []
+
+#diff_2_1_line = []
+
+
+comp_array1.each_with_index do |val, idx|
+
+diff_1_2_line = []
+
+ 
+  #Some runtime output which may get tossed in final version
+  puts comp_array2.include?(val).to_s + " " + val + " ID: " + idx.to_s
+  
+  
+  if comp_array2.include?(val)
+    match_number += 1
+     
+  else
+    sheet1.header_array.each_with_index do |h_val, i|
+      
+      diff_1_2_line[i] = sheet1.result_array[idx.+ 1][h_val]
+    end  
+  end
+ 
+
+  diff_1_2[idx + 1] =  diff_1_2_line
+ 
+
+ 
+
+ 
+
+
+
+
+
+  #end comp_array1.each
 end
 
-pp  "ONE" , comp_array1
-pp "TWO" , comp_array2
+#pp diff_1_2
+
+#=begin
+writer = CSV.open('./results/diff_1-2.csv', 'w')
+diff_1_2.each do |line|
+  writer << line
+end
+writer.close
+#=end
 
 
-# comp_array1.each_with_index do |val , idx|
-#   if comp_array2.include?(val)
-#     p "It's here"
-#     pp val
-#     pp comp_array1.index(val)
-#     pp comp_array2.index(val)
-#   else
-#     p "It's not"
-#     pp val
-#     pp comp_array1.index(val)
-#     pp comp_array2.index(val)
-   
-#   end
-# end
+
+
+#sheet2.result_array[n][key2]   Returns the IP in the n^th row
+
+puts "Number of entries from first, in second: " +  match_number.to_s
+
+
+#pp "Union" , union_array
+#diff_1_2 , diff_2_1
+#pp "Head Array" , sheet1.header_array
+ 
+ 
+
